b2012419351123ad1b0fd8acec12bcef12e3cdb1
mbr238 <mbr238@nau,edu>
2022-05-10 15:15:57 -0700

    header file for inserting function

---
+++ converted files/main.h
@@ -0,0 +1,52 @@
+// Preprocessor directive
+#ifndef main_H
+#define main_H
+
+
+int importDataset(char * fname, int N, double ** dataset)
+{
+
+    FILE *fp = fopen(fname, "r");
+
+    if (!fp) {
+        printf("Unable to open file\n");
+        return(1);
+    }
+
+    char buf[4096];
+    int rowCnt = 0;
+    int colCnt = 0;
+    while (fgets(buf, 4096, fp) && rowCnt<N) {
+        colCnt = 0;
+
+        char *field = strtok(buf, ",");
+        double tmp;
+        sscanf(field,"%lf",&tmp);
+        dataset[rowCnt][colCnt]=tmp;
+
+        
+        while (field) {
+          colCnt++;
+          field = strtok(NULL, ",");
+          
+          if (field!=NULL)
+          {
+          double tmp;
+          sscanf(field,"%lf",&tmp);
+          dataset[rowCnt][colCnt]=tmp;
+          }   
+
+        }
+        rowCnt++;
+    }
+
+    fclose(fp);
+    return 0;
+
+
+}
+
+
+
+
+#endif  // end main_H
