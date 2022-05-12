9ce5b14b693a810285482a56107935819d622f7b
mbr238 <mbr238@nau.edu>
2022-05-12 09:18:10 -0700

    updated header file for functions

--- converted files/main.h
+++ converted files/main.h
@@ -2,49 +2,113 @@
 #ifndef main_H
 #define main_H
 
+struct Node{
+	int value;
+	int begin;
+	int end;
+	
+	struct Node *leftChild;
+	struct Node *rightChild;
+};
 
-int importDataset(char * fname, int N, double ** dataset)
-{
+struct Tabke{
+	int numRows;
+	int numCols;
+	char *name;
+	bool hasLabel;
+	Instance *instance;
+	
+};
 
-    FILE *fp = fopen(fname, "r");
+struct Hypercube{
+	Table *X;
+	int length;
+	int Wmax;
+	
+};
 
-    if (!fp) {
-        printf("Unable to open file\n");
-        return(1);
-    }
+//node fnctions
+Node * create_node(int value, int begin, int end); // you can use a tree with pointers instead of HashMap
 
-    char buf[4096];
-    int rowCnt = 0;
-    int colCnt = 0;
-    while (fgets(buf, 4096, fp) && rowCnt<N) {
-        colCnt = 0;
+//functions from dr.Gowanlock
+int importDataset(char * fname, int N, double ** dataset);
 
-        char *field = strtok(buf, ",");
-        double tmp;
-        sscanf(field,"%lf",&tmp);
-        dataset[rowCnt][colCnt]=tmp;
+//table fnctions
 
-        
-        while (field) {
-          colCnt++;
-          field = strtok(NULL, ",");
-          
-          if (field!=NULL)
-          {
-          double tmp;
-          sscanf(field,"%lf",&tmp);
-          dataset[rowCnt][colCnt]=tmp;
-          }   
+Table * create_table(int numRows, int numcols, Instance* instance, char* name, bool hasLabel); // this function works like a constructor
 
-        }
-        rowCnt++;
-    }
+int get_num_rows(Table * X);
 
-    fclose(fp);
-    return 0;
+int get_num_cols(Table * X);
 
 
+//TreeStrategy functions
+int density(int i, Node parent, int col);
+
+int * get_densities(Hypercube * H, int n, int minSplit) {
+    Node root = create_node(-1, 0, H.length -1)
+
+    # code of TreeStrategy.getDensities() goes here
 }
+
+//HySortOD functions
+
+double * get_outlierness_score(Hypercube * H, int * W, int m);
+
+double score(int density);
+
+double * hysortod_score(Table * X, int b, int minSplit) {
+   double l = 1 / (double) b;
+   m = get_num_rows(X);
+
+   Hypercube * H = get_sorted_hypercubes(X);
+   int * W = get_densities(H, minSplit);
+   double * scores = get_outlierness_score(H, W, m)
+
+   return scores
+}
+
+//Idesity strategy functions
+typedef struct IdensityStrategy{
+	IdensityStrategy buildIndex(Hypercube *H);
+	int *getDensities();
+	double getMaxDensity();
+	
+};
+
+
+//Instance fnctions
+
+struct Instance{
+	int id;
+	int label;
+	bool hasLabel;
+	double *values;
+};
+
+   Instance *new_Instance( int id, double *values, int label);
+
+   int getId(Instance *self);
+
+   double *getValues(Instance *self);
+
+   double getAt(int j);
+   
+   void setAt(int j, double value);
+   
+   int getLabel(Instance *self);
+   
+   bool hasLabel(Instance *self);
+   
+   int getNumDimensions(Instance *self);
+
+
+	//naiveStrategy functions
+
+
+	IdensityStrategy buildIndex(Hypercube *H);
+
+	int *getDensities(Hypercube *H);
 
 
 
