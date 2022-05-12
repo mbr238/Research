c68c960ef265b5241c1ca0a62f34ea77c269bad3
mbr238 <mbr238@nau,edu>
2022-05-12 12:50:35 -0700

    updated -- still needs more

--- converted files/main.h
+++ converted files/main.h
@@ -1,31 +1,53 @@
 // Preprocessor directive
 #ifndef main_H
 #define main_H
+#include <stdbool.h>
 
-struct Node{
+//declared types/structs
+typedef enum { IDensityStrategy, TreeStrategy } strategy;
+
+typedef struct IdensityStrategy{
+	int **buildIndex;
+	int *Densities;
+	double MaxDensity;
+	int Wmax;
+	
+}IdensityStrategy;
+//need functions for these 3 above ^^
+
+typedef struct Node{
 	int value;
 	int begin;
 	int end;
 	
 	struct Node *leftChild;
 	struct Node *rightChild;
-};
+}Node;
 
-struct Tabke{
+typedef struct Instance{
+	int id;
+	int label;
+	bool hasLabel;
+	double *values;
+}Instance;
+
+typedef struct Table{
 	int numRows;
 	int numCols;
 	char *name;
 	bool hasLabel;
 	Instance *instance;
 	
-};
+}Table;
 
-struct Hypercube{
+typedef struct Hypercube{
 	Table *X;
 	int length;
 	int Wmax;
 	
-};
+}Hypercube;
+
+
 
 //node fnctions
 Node * create_node(int value, int begin, int end); // you can use a tree with pointers instead of HashMap
@@ -46,9 +68,10 @@ int get_num_cols(Table * X);
 int density(int i, Node parent, int col);
 
 int * get_densities(Hypercube * H, int n, int minSplit) {
-    Node root = create_node(-1, 0, H.length -1)
+    Node root = create_node(-1, 0, H->length - 1);
 
-    # code of TreeStrategy.getDensities() goes here
+    // code of TreeStrategy.getDensities() goes here
+	return 0;
 }
 
 //HySortOD functions
@@ -59,32 +82,19 @@ double score(int density);
 
 double * hysortod_score(Table * X, int b, int minSplit) {
    double l = 1 / (double) b;
-   m = get_num_rows(X);
+   int m = get_num_rows(X);
 
    Hypercube * H = get_sorted_hypercubes(X);
-   int * W = get_densities(H, minSplit);
-   double * scores = get_outlierness_score(H, W, m)
+   int * W = get_densities(H, l, minSplit);
+   double * scores = get_outlierness_score(H, W, m);
 
-   return scores
+   return scores;
 }
 
-//Idesity strategy functions
-typedef struct IdensityStrategy{
-	IdensityStrategy buildIndex(Hypercube *H);
-	int *getDensities();
-	double getMaxDensity();
-	
-};
 
 
 //Instance fnctions
 
-struct Instance{
-	int id;
-	int label;
-	bool hasLabel;
-	double *values;
-};
 
    Instance *new_Instance( int id, double *values, int label);
 
@@ -111,6 +121,24 @@ struct Instance{
 	int *getDensities(Hypercube *H);
 
 
+//Auc functions
+
+double AUC_measure(int *truth, double *probability);
+
+//abstract density strategy functions
+struct AbstractDensityStrategy{
+	int Wmax;
+	
+	Hypercube *H;
+	
+};
+
+double getMaxDensity(Hypercube *self);
+
+bool isImmediate(Hypercube hi, Hypercube hk);
+
+
+bool isProspective(Hypercube hi, Hypercube hk, int col);
 
 
 #endif  // end main_H
