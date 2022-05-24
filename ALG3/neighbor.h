41a4e758f24ccbc2511dbc6787618b99c7a19215
mbr238 <mbr238@nau.edu>
2022-05-24 09:47:23 -0700

    updated for scanning hypercube array instead of binary tree

--- ALG3/neighbor.h
+++ ALG3/neighbor.h
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 #include <math.h>
 
-
 bool isImmediate(Hypercube *H, Hypercube *F)
 {
 	if( abs(H->x_coords - F->x_coords > 1) || abs(H->y_coords - F->y_coords > 1))
@@ -20,55 +19,38 @@ bool isProspective(Hypercube *H, Hypercube *F, int col)
 	return abs(H->y_coords - F->y_coords ) <= 1;
 }
 
-bool isLeaf(node *H)
-{
-	if( H->leftchild == NULL && H->rightchild == NULL)
-	{
-		return true;
-	}
-	
-	return false;
-}
 
 
-double neighborhood_density(Hypercube *array, Hypercube *H, Hypercube *root, int position, int dimension)
+double neighborhood_density(Hypercube *array, Hypercube *H, node *root, int position, int dimension)
 {
 	//initialize program
 	int density = 0, countings = 0;
+	int dimensions = dimension;
 	
 	//processing
 		//initialize density with the first density/hypercube countins
 		countings = H->countings;
 	
-		//check if current location in root is a leaf
-		if( isLeaf( root))
-		{
-			//from begin to end
-			for(int k = root->begin; k < root->end; k++)
+			//traverse the hypercube array
+			for(int k = 0; k < b; k++)
 			{
-				//check if the hypercubes are immediate
-				if(	isImmediate(H, root[k]))
+				for(int j = 0; j < b; j++)
 				{
-					//if they are then add that cubes contings to the original density
-					density = density + root[k]->countings;
-				}				
-				//end if loop
-				
+				if(array[k][j] != NULL)
+				{
+					//check if the hypercubes are immediate
+					if(	isImmediate(H, array[k][j]))
+					{
+						//if they are then add that cubes contings to the original density
+						density = density + array[k][j]->countings;
+					}				
+					//end if loop
+				}
+				}
 			}
 			//end for loop
-		}
-		//else
-		else
-		{			
-				//for each node within hypercube array that map the hypercubbes coords +- 1
-				
-					//density is recursively calling the neighborhood density function
-					
-				//end for
-				
-		} //end else
 	//return the neighborhood density
-	
+	return density;
 }
 
 
