529184bb514553e643863d9ace36cde7a3da8a63
Michael Rocha <101938514+mbr238@users.noreply.github.com>
2022-05-31 08:32:51 -0700

    Delete files to convert directory

--- files to convert/IDensityStrategy.java
+++
@@ -1,9 +0,0 @@
-package common.search;
-
-import common.Hypercube;
-
-public interface IDensityStrategy {
-	public IDensityStrategy buildIndex(Hypercube[] H);
-	public int[] getDensities();
-	public double getMaxDensity();
-}
