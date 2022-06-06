1c1ccb316eb935532e8ca211e2757d5f37ae252f
mbr238 <mbr238@nau,edu>
2022-06-06 12:52:12 -0700

    updated for nth dimensions

--- ALG4/neighbor.h
+++ ALG4/neighbor.h
@@ -7,16 +7,15 @@
 //function to calculate neighboring hypercubes
 bool isImmediate(Hypercube *H, Hypercube *F)
 {
-	if( abs(H->coords[0] - F->coords[0]) <= 1 && abs(H->coords[1] - F->coords[1]) <= 1)
-	{
-		return true;
-	}
+	//initialize varaibles
+	bool neighborFlag = false;
 
-	return false; // they are not neighbors
+
+	return neighborFlag;
 }
 
 
-DTYPE neighborhood_density(Hypercube* array[][b], Hypercube *H)
+DTYPE neighborhood_density(Hypercube **array, Hypercube *H)
 {
 	//initialize program
 	DTYPE density = 0;
@@ -26,25 +25,18 @@ DTYPE neighborhood_density(Hypercube* array[][b], Hypercube *H)
 		density = H->countings;
 		
 			//traverse the hypercube array
-			for(int k = 0; k < b; k++)
+			for(int k = 0; k <= pow(b,DIM); k++)
 			{
-				for(int j = 0; j < b; j++)
-				{
-				if(array[k][j] != NULL)
-				{
 					//check if the hypercubes are immediate
-					if( array[k][j] != H && isImmediate(H, array[k][j]))
+					if( array[k] != NULL && array[k] != H && isImmediate(H, array[k]) )
 					{
 						//if they are then add that cubes contings to the original density
-						density = density + array[k][j]->countings;
+						density = density + array[k]->countings;
 						
 					}				
 					//end if loop
-					
-				}
-				}
 			}
-
+			
 			//end for loop
 	//return the neighborhood density
 	return density;
