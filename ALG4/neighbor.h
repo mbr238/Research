d9eeff0324caae8c598fc59ea61f2a969fb253fa
mbr238 <mbr238@nau,edu>
2022-06-21 06:00:03 -0700

    updated

--- ALG4/neighbor.h
+++ ALG4/neighbor.h
@@ -6,6 +6,15 @@
 #include <stdbool.h>
 #include "params.h"
 
+
+bool isProspective(Hypercube *H, Hypercube *F)
+{
+	return (abs(H->coords[0] - F->coords[0]) <= 1);
+	
+}
+
+
+
 //function to calculate neighboring hypercubes
 bool isImmediate(Hypercube *H, Hypercube *F)
 {
@@ -32,16 +41,44 @@ DTYPE neighborhood_density(Hypercube **array, Hypercube *H, int N)
 		//initialize density with the first density/hypercube countins
 		density = H->countings;
 		
+		
+		
 			//traverse the hypercube array
 			for(int k = 0; k < N; k++)
 			{
+				for(int i = k - 1; i >= 0; i--)
+				{
+					if(!isProspective(array[k], H))
+					{
+						break;
+					}
 					//check if the hypercubes are immediate
 					if( array[k] != NULL && array[k] != H && isImmediate(H, array[k]) )
 					{
 						//if they are then add that cubes contings to the original density
 						density = density + array[k]->countings;
 						
-					}				
+					}	
+				}
+
+				for(int i = 0; i >= 0; i++)
+				{
+					if(!isProspective(array[k], H))
+					{
+						break;
+					}
+					//check if the hypercubes are immediate
+					if( array[k] != NULL && array[k] != H && isImmediate(H, array[k]) )
+					{
+						//if they are then add that cubes contings to the original density
+						density = density + array[k]->countings;
+						
+					}	
+				}
+
+
+
+				
 					//end if loop
 			}
 			
