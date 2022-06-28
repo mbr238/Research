c79e1fae13624e8e6768b9f09415a2c993616fe0
mbr238 <mbr238@nau.edu>
2022-06-28 09:28:10 -0700

    sort for function

--- ALG4/sort.h
+++ ALG4/sort.h
@@ -1,7 +1,57 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stdbool.h>
 #include "hypercube.h"
+
+void swap( Hypercube **arr, int i, int j)
+{
+	Hypercube *a;
+	a = arr[i];
+	arr[i] = arr[j];
+	arr[j] = a;
+}
+
+bool compareCubes(Hypercube *H, Hypercube *F)
+{
+	
+	for(int i = 0; i < DIM; i++)
+	{
+		if(H->coords[i] < F->coords[i] )
+		{
+			return true;
+		}
+		
+	}
+	return false;
+}
+
+
+void sort(Hypercube **array, int N)
+{
+	
+		for(int i = 0; i < N; i++)
+		{
+			if(array[i] == NULL)
+			{
+				break;
+			}
+			for(int j = 1; j < N; j++)
+			{
+				if(array[j] == NULL)
+				{
+					break;
+				}
+				if(compareCubes(array[i], array[j]))
+				{
+					swap(array, i, j);
+				}
+			}
+		
+		}
+	
+}
+
+
 
 //sort the hypercubes in ascending order
 
