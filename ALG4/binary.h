f3661bd50a2896b897d6a14c8237212d85af8c04
map759 <72110319+map759@users.noreply.github.com>
2022-08-04 08:20:57 -0700

    Update binary.h

--- ALG4/binary.h
+++ ALG4/binary.h
@@ -7,7 +7,24 @@ typedef struct node{
 	
 }node;
 
+bool sort(Hypercubes **array, int N) {
+	for (i = 0; i < N; i++) {
+		int low = 0;
+		int high = n -1;
+		int mid = (low + high)/2;
+		while (low <= high ) {
+			if (Hypercubes **array == Key) {
+				low = mid + 1;
+			}
+			else if (Hypercubes **array == Key) {
+				
+			}
+		}
+	}
+	
+}
 
+/* This not finished but it's start. It won't run becasue I don't have a "key" */
 
 
 
