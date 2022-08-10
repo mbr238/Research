0421683d2b72674c2896058c88bc93cb6b4c422e
map759 <72110319+map759@users.noreply.github.com>
2022-08-10 16:09:59 -0700

    Update binary.h

--- ALG4/binary.h
+++ ALG4/binary.h
@@ -1,38 +1,35 @@
 #ifndef BINARY_H
 #define BINARY_H
 
+//header files
+#include "hypercube.h"
+#include <stdbool.h>
+
 
 typedef struct node{
 	
-	
 }node;
 
-bool sort(Hypercubes **array, int N) {
+    int i;
+
+// If the hypercube is NULL
+bool sort(Hypercube **array, int N) {
 	for (i = 0; i < N; i++) {
 		int low = 0;
 		int high = 0;
 		int node;
 		while (low <= high ) {
-			mid = (low + high) / 2;
-			if (Hypercubes **array == Key) 
-			{
+            int mid = (low + high) / 2;
+			if (Hypercube **array >= Key) {
 				low = mid + 1;
 			}
-			else if (Hypercubes **array == Key) 
-			{
-				high = mid - 1; 
+			else if (Hypercube **array <= Key) {
+				high = mid - 1;
 			}
 		}
 	}
 	
 }
-
-/* This not finished but it's start. It won't run becasue I don't have a "key" 
-
-So the "key" is the coord and now I just have to replace but instead of "==" we need to use "<", ">" 
-
-I am curretnly working with the Python Hysord */
-
 
 
 
